35a755e31eba276495f3f59ecdaa9399c27a8d3f
Paulo Cesar <paulocesarpinheirodasilva0@gmail.com>
2025-03-16 00:53:42 -0300

    feat: add Main class with Calculator usage example

--- src/main/java/com/paulomlr/Main.java
+++ src/main/java/com/paulomlr/Main.java
@@ -2,6 +2,10 @@ package com.paulomlr;
 
 public class Main {
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator();
+        System.out.println("Sum: " + calculator.add(2, 3));
+        System.out.println("Subtract: " + calculator.subtract(5, 2));
+        System.out.println("Multiply: " + calculator.subtract(4, 3));
+        System.out.println("Divide: " + calculator.divide(6, 2));
     }
 }
